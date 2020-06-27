a120ec582742bfb3daff34658bbdfe6e56ab74a8
john <thdwmdgns@naver.com>
2020-06-27 16:59:03 +0900

    commit1

--- deep-learning-from-scratch-master/gradient_simplenet.py
+++ deep-learning-from-scratch-master/gradient_simplenet.py
@@ -1,22 +1,22 @@
 # coding: utf-8
 import sys, os
-sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+
 import numpy as np
-from common.functions import softmax, cross_entropy_error
-from common.gradient import numerical_gradient
+from common1 import functions as func
+from common1 import gradient as grad
 
 
 class simpleNet:
     def __init__(self):
         self.W = np.random.randn(2,3) # 정규분포로 초기화
-
+        
     def predict(self, x):
         return np.dot(x, self.W)
 
     def loss(self, x, t):
         z = self.predict(x)
-        y = softmax(z)
-        loss = cross_entropy_error(y, t)
+        y = func.softmax(z)
+        loss = func.cross_entropy_error(y, t)
 
         return loss
 
@@ -26,6 +26,6 @@ t = np.array([0, 0, 1])
 net = simpleNet()
 
 f = lambda w: net.loss(x, t)
-dW = numerical_gradient(f, net.W)
+dW = grad.numerical_gradient(f, net.W)
 
 print(dW)
--- deep-learning-from-scratch-master/myfunctions.py
+++ deep-learning-from-scratch-master/myfunctions.py
@@ -28,11 +28,11 @@ def relu_grad(x):
     return grad
     
 
-def softmax(x):
+def softmax(x): #softmax function () 활성화함수
     if x.ndim == 2:
         x = x.T
         x = x - np.max(x, axis=0)
-        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0) 
         return y.T 
 
     x = x - np.max(x) # 오버플로 대책
