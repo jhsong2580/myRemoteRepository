2f6f3b70b92eebbeed4c8b31628c77d5142d958b
john <thdwmdgns@naver.com>
2020-07-29 05:40:30 +0000

    commit

--- nodejs/main.js
+++ nodejs/main.js
@@ -3,17 +3,20 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 function templateHTML(path){
-    fs.readFile(path,'utf-8',function(error,data){
-        return data;
-    });
+	data = fs.readFileSync(path,'utf-8');
+	return data;
 }
 
 var app = http.createServer(function(request,response){
+   var _url = request.url;
+   var pathname = url.parse(_url,true).pathname;
 
     if(pathname == '/'){
          response.writeHead(200);
-         response.end(templateHTML('./html/index.html'));
+		 var template = templateHTML('./html/index.html');
+		 console.log(template);
+         response.end(template);
     }
 })
 
-app.listen(3000);
+app.listen(9000);
